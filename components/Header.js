3e4257b0bd4905e131c2262df96f2a698c137a2e
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-08 15:32:05 -0800

    Import solid icons

--- components/Header.js
+++ components/Header.js
@@ -32,7 +32,8 @@ function Header() {
         <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
         <SearchIcon  className='h-5 w-5 text-gray-500'/>
         </div>
-        <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black' 
+        <input 
+        className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' 
         type="text" 
         placeholder='Search'/>
       </div>
