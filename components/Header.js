a7b24a6ab1905ee7ee1fab341d5faff49622ac5b
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-08 14:32:18 -0800

    install tailwaind/forms and update tailwind.config

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ function Header() {
     <div>
       <div className='flex justify-between max-w-6xl'>
       {/* {Left} */}
-      <div className='relative hidden lg:inline-grid h-24 w-24 cursor-pointer'>
+      <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
         <Image src='http://links.papareact.com/ocw' 
         layout='fill'
         objectFit='contain'/>
@@ -28,11 +28,13 @@ function Header() {
 
 
       {/* {Middle - Search input field} */}
-      <div className='mt-1'>
+      <div className='mt-1 p-3 rounded-md'>
         <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
         <SearchIcon  className='h-5 w-5 text-gray-500'/>
         </div>
-        <input type="text" placeholder='Search'/>
+        <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black' 
+        type="text" 
+        placeholder='Search'/>
       </div>
 
 
