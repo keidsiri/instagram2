86b9ca831759ce9cea98fe948d944f0d63074a3f
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-08 21:29:31 -0800

    Add Posts.js and Post.js

--- components/Feed.js
+++ components/Feed.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import Posts from './Posts'
 import Stories from './Stories'
 
 function Feed() {
   return (
-    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto'>
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
         <section className='col-span-2'>
-        {/* {Stories} */}
-        <Stories />
 
-        {/* {Posts} */}
+        <Stories />
+        <Posts />
         </section>
         
         <section>
---
+++ components/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Posts() {
+  return (
+    <div>
+      <Post />
+
+
+    </div>
+  )
+}
+
+export default Posts
