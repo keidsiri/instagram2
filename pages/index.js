38f34f341f2f04ee4acddb4c80dc2dbdf82345b1
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-08 21:19:36 -0800

    update scroll bar

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram 2.0</title>
         <link rel="icon" href="/favicon.ico" />
