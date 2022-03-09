2fc23e053d20b15dabaaa390a663330f2208e32c
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-08 20:51:28 -0800

    Styling the stories

--- components/Story.js
+++ components/Story.js
@@ -3,8 +3,12 @@ import React from 'react'
 function Story({img, username}) {
   return (
     <div>
-      <img src={img} alt="image"/>
-      <p>{username}</p>
+      <img 
+      className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer 
+      hover:scale-110 transition transform duration-200 ease-out' 
+      src={img} 
+      alt="image"/>
+      <p className='text-xs w-14 truncate text-center'>{username}</p>
     </div>
   )
 }
