77656facedc7221ed2316b5a9d6060c75e6295bd
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-09 14:03:17 -0800

    Update Nextauth.js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/Google"
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.Google_CLIENT_ID,
+      clientSecret: process.env.Google__CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+}
