67ba74f0eb936368402b5891f5ef726eb4b50b97
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-08 17:29:40 -0800

    Add Stories.js

---
+++ components/Stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import faker from "faker";
+
+function Stories() {
+
+  
+  return (
+    <div>
+      {/* {Stories} */}
+      {/* {Stories} */}
+      {/* {Stories} */}
+      {/* {Stories} */}
+      {/* {Stories} */}
+    </div>
+  )
+}
+
+export default Stories;
