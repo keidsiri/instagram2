8a9b4314f78a7ecb60c3962749c3fae72cd3e6c8
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-10 01:31:04 -0800

    Add Transition.Child in Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition } from '@headlessui/react'
@@ -6,9 +6,39 @@ import { Dialog, Transition } from '@headlessui/react'
 function Modal() {
   const [open ,setOpen] = useRecoilState(modalState);
 
-  return <div>
-    <h1></h1>
-  </div>;
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as='div' className='fixed z-10 inset-0 overflow-auto' onClose={setOpen}>
+        <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+        <Transition.Child 
+        as={Fragment} 
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0">
+          <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        {/*this element is to trick the browser into centering the model content */}
+        <span className='hidden sm:inline-block sm:align-middle sm:h-screen' aria-hidden="true"> &#8203; </span>
+
+        <Transition.Child 
+        as={Fragment} 
+        enter="ease-out duration-300"
+        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        enterTo="opacity-100 translate-y-o sm:scale-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+        
+        </Transition.Child>
+        </div>
+      </Dialog>
+    
+    </Transition.Root >
+  );
 }
 
 export default Modal;
