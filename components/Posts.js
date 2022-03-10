98263e371a6d51f5c56ee6ec0ed93ee4cf949c82
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-10 11:21:26 -0800

    Succesful pass to uplaod picture and cation on Posts.js

--- components/Posts.js
+++ components/Posts.js
@@ -1,5 +1,6 @@
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebase';
 import Post from './Post';
 
 const posts = [
@@ -35,10 +36,10 @@ function Posts() {
         <Post 
         key={post.id}
         id={post.id}
-        username={post.username}
-        userImg={post.userImg}
-        img={post.img}
-        caption={post.caption}
+        username={post.data().username}
+        userImg={post.data().profileImg}
+        img={post.data().image}
+        caption={post.data().caption}
         />
       ))}
     </div>
