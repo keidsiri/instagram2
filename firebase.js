3efdc301f1a3420dcc51f7fb0a87ac9db9821e05
Kate Kiatsiri <keidsiri@gmail.com>
2022-03-09 16:25:17 -0800

    Create .env ,place Google Client ID and Secret in the file

--- firebase.js
+++ firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp , getApp, getApps } from "firebase/app";
+import { get } from "http";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +20,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !get.Apps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+// const analytics = getAnalytics(app);
+
+export {app, db, storage}; 
